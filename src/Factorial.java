a0ab412e4bc703de1ba1720a4fb77cd654252425
Unish Rajak <unishrajak0@gmail.com>
2024-11-22 00:07:56 +0545

    Factorial

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Factorial {
+    public static void main(String[] args) {
+
+        try {
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("Enter a number: ");
+            int n = sc.nextInt();
+            sc.close();
+            int fact = 1;
+
+            for (int i = 1; i <= n; i++) {
+                fact = fact * i;
+            }
+
+            System.out.println("Factorial of " + n + " is " + fact);
+        }
+
+        catch (InputMismatchException e) {
+            System.out.println("Please enter a valid number");
+        }
+
+
+
+    }
 }
