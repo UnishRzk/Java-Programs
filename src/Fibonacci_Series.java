ab5e70ca85e8960bc83ef8cc9cf8a74d84656b64
Unish Rajak <unishrajak0@gmail.com>
2024-11-21 22:57:17 +0545

    Palindrome number

--- src/Fibonacci_Series.java
+++ src/Fibonacci_Series.java
@@ -1,6 +1,5 @@
 public class Fibonacci_Series {
-
-    Fibonacci_Series() {
+    public static void main(String[] args) {
 
         int n1 = 0, n2 = 1;
         System.out.print(n1+ " " +n2);
@@ -13,5 +12,6 @@ public class Fibonacci_Series {
         }
 
     }
+
 
 }
